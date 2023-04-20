7d4ae7005b4487f10938f3c290d1b66a8244634a
inchoxd <rjb2066@gmail.com>
2023-04-20 10:44:32 +0900

    added a argument of main fuction

--- imread.cpp
+++ imread.cpp
@@ -1,9 +1,9 @@
 #include <cstdio>
 #include <opencv2/opencv.hpp>
 
-int main(i) {
+int main(int argc, char *argv[]) {
   cv::Mat image;
-  image = cv::imread("barbara.pgm");
+  image = cv::imread(argv[1]);
   if (image.empty()) {
     printf("Image file is not found.\n");
     return EXIT_FAILURE;
