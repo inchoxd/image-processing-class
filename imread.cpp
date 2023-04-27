a5de00b217edad70ad7f58a5d98e5cc8d3df4a65
inchoxd <rjb2066@gmail.com>
2023-04-27 10:22:31 +0900

    added a variable f_path, added a function about checking filepath is null.

--- imread.cpp
+++ imread.cpp
@@ -2,8 +2,12 @@
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char *argv[]) {
+  const char *f_path = argv[1];
+  if(f_path == NULL)
+      f_path = "barbara.pgm";
+
   cv::Mat image;
-  image = cv::imread(argv[1]);
+  image = cv::imread(f_path);
   if (image.empty()) {
     printf("Image file is not found.\n");
     return EXIT_FAILURE;
