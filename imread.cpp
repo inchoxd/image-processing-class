26cba642258d694f89e159b1048184ec4037851a
inchoxd <rjb2066@gmail.com>
2023-05-11 10:55:38 +0900

    added a process of blacken the upper left corner

--- imread.cpp
+++ imread.cpp
@@ -10,12 +10,21 @@ int main(int argc, char *argv[]) {
   }
 
   cv::Mat image;
-  image = cv::imread(f_path);
+  image = cv::imread(f_path, cv::ImreadModes::IMREAD_GRAYSCALE);
   if(image.empty()) {
     printf("Image file is not found.\n");
     return EXIT_FAILURE;
   }
   printf("width=%d, height=%d\n", image.cols, image.rows);
+  for(int y = 0; y < image.rows; ++y) {
+      for(int x = 0; x < image.cols; ++x) {
+          if(x < image.cols / 2 && y < image.rows / 2) {
+              int val = image.data[y * image.cols + x];       // x, y座標を指定して画素を取得．ストライド．
+              val /= 2;
+              image.data[y * image.cols + x] = val;
+          }
+      }
+  }
   cv::imshow("image", image);
   cv::waitKey();
   cv::destroyAllWindows();
