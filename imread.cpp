4bc7561626263dfe03da37737a112f99f16d4c64
inchoxd <rjb2066@gmail.com>
2023-05-15 18:01:27 +0900

    added a new feature of to tile images.

--- imread.cpp
+++ imread.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 
@@ -90,7 +92,7 @@ cv::Mat blacken_upper_left_corner(cv::Mat data) {
 }
 
 int main(int argc, char *argv[]) {
-    int i = 0;
+    int i = 0, width = 0, height = 0;
     const char *f_path = argv[1];
 
     if(argc < 2 && f_path == NULL) {
@@ -99,34 +101,39 @@ int main(int argc, char *argv[]) {
     }
 
     cv::Mat input, data, image[3];
-        input = cv::imread(f_path, cv::ImreadModes::IMREAD_GRAYSCALE);
+    input = cv::imread(f_path, cv::ImreadModes::IMREAD_GRAYSCALE);
+    cv::Mat tiled(input.rows * 2, input.cols * 2, input.type());
     if(input.empty()) {
         printf("Image file is not found.\n");
         return EXIT_FAILURE;
     }
-    printf("width=%d, height=%d\n", input.cols, input.rows);
-    cv::imshow("input_data", input);
+
+    width = input.cols;
+    height = input.rows;
+    printf("width=%d, height=%d\n", width, height);
+    input.copyTo(tiled(cv::Rect(0, 0, width, height)));
 
     while(i < 3) {
         data = cv::imread(f_path, cv::ImreadModes::IMREAD_GRAYSCALE);
         switch(i) {
             case 0:
-                image[0] = checkered_flag(data);
-                cv::imshow("checkered_flag", image[0]);
+                image[0] = blacken_upper_left_corner(data);
+                image[0].copyTo(tiled(cv::Rect(width, 0, width, height)));
                 break;
             case 1:
                 image[1] = zebra(data);
-                cv::imshow("zebra", image[1]);
+                image[1].copyTo(tiled(cv::Rect(0, height, width, height)));
                 break;
             case 2:
-                image[2] = blacken_upper_left_corner(data);
-                cv::imshow("blacken_upper_left_corner", image[2]);
+                image[2] = checkered_flag(data);
+                image[2].copyTo(tiled(cv::Rect(width, height, width, height)));
                break; 
         }
         i++;
     }
-    //image = zebra(input);
-    //image = blacken_upper_left_corner(input);
+
+    cv::resize(tiled, tiled, cv::Size(), 0.65, 0.65);
+    cv::imshow("results", tiled);
     cv::waitKey();
     cv::destroyAllWindows();
 
