66422a2477afe67fe40781e3da27db446fd4a4e7
inchoxd <rjb2066@gmail.com>
2023-05-15 14:56:32 +0900

    created a new function about generating zebra pattern.

--- imread.cpp
+++ imread.cpp
@@ -2,32 +2,71 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 
+cv::Mat zebra(cv::Mat input) {
+    bool fill = true;
+    int a = 0, b = 0, tmp = 0;
+    int x = input.cols, y = input.rows;
+
+    a = x;
+    b = y;
+    while((tmp = a % b) != 0) {
+        a = b;
+        b = tmp;
+    }
+
+    for(int y = 0; y < input.rows; ++y) {
+        for(int x = 0; x < input.cols; ++x) {
+            if(fill == true) {
+                int val = input.data[y * input.cols + x];       // x, y座標を指定して画素を取得．ストライド．
+                val = 0;
+                input.data[y * input.cols + x] = val;
+            }
+            if(x % b == 0) {
+                if(fill == true) {
+                    fill = false;
+                } else {
+                    fill = true;
+                }
+            }
+        }
+    }
+
+    return input;
+}
+
+cv::Mat blacken_upper_left_corner(cv::Mat input) {
+    for(int y = 0; y < input.rows; ++y) {
+        for(int x = 0; x < input.cols; ++x) {
+            if(x < input.cols / 2 && y < input.rows / 2) {
+                int val = input.data[y * input.cols + x];       // x, y座標を指定して画素を取得．ストライド．
+                val = 0;
+                input.data[y * input.cols + x] = val;
+            }
+        }
+    }
+
+    return input;
+}
+
 int main(int argc, char *argv[]) {
-  const char *f_path = argv[1];
-  if(argc < 2 && f_path == NULL) {
-      printf("input file_path >>");
-      scanf("%s", f_path);
-  }
+    const char *f_path = argv[1];
+    if(argc < 2 && f_path == NULL) {
+        printf("input file_path >>");
+        scanf("%s", f_path);
+    }
 
-  cv::Mat image;
-  image = cv::imread(f_path, cv::ImreadModes::IMREAD_GRAYSCALE);
-  if(image.empty()) {
-    printf("Image file is not found.\n");
-    return EXIT_FAILURE;
-  }
-  printf("width=%d, height=%d\n", image.cols, image.rows);
-  for(int y = 0; y < image.rows; ++y) {
-      for(int x = 0; x < image.cols; ++x) {
-          if(x < image.cols / 2 && y < image.rows / 2) {
-              int val = image.data[y * image.cols + x];       // x, y座標を指定して画素を取得．ストライド．
-              val = 0;
-              image.data[y * image.cols + x] = val;
-          }
-      }
-  }
-  cv::imshow("image", image);
-  cv::waitKey();
-  cv::destroyAllWindows();
+    cv::Mat data, image;
+    data = cv::imread(f_path, cv::ImreadModes::IMREAD_GRAYSCALE);
+    if(data.empty()) {
+        printf("Image file is not found.\n");
+        return EXIT_FAILURE;
+    }
+    printf("width=%d, height=%d\n", data.cols, data.rows);
+    image = zebra(data);
+    //image = blacken_upper_left_corner(data);
+    cv::imshow("image", data);
+    cv::waitKey();
+    cv::destroyAllWindows();
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
