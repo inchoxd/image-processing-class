ee5e5cdef82a5bf228a8de51b45d72f3b85d1ec7
inchoxd <rjb2066@gmail.com>
2023-05-25 10:00:17 +0900

    refactored a function of cvtYCbCr

--- imread.cpp
+++ imread.cpp
@@ -5,39 +5,28 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-typedef struct {
-    int B;
-    int G;
-    int R;
-} BGR;
-
-typedef struct {
-    int Y;
-    int Cb;
-    int Cr;
-} YCbCr;
-
 int cvtYCbCr(cv::Mat &data) {
     const int w = data.cols, h = data.rows, NC = data.channels();
+    // const int STRIDE = w * NC;
     if(NC < 3)
         return 1;
 
+    uchar *p0, *p1, *p2;
+    p0 = data.data;
+    p1 = data.data + 1;
+    p2 = data.data + 2;
+
     for (int y = 0; y < h; ++y) {
-        const int STRIDE = w * NC;
-        for (int x = 0; x < w; ++x) {
-            double out[3] = {0.0, 0.0, 0.0};
-            double wY[3] = {0.114, 0.587, 0.299};
-            double wCb[3] = {0.5, -.3313, -.1687};
-            double wCr[3] = {-.0813, -.4187, 0.5};
-            for (int c = 0; c < NC; ++c) {
-                out[0] += data.data[y * STRIDE + x * NC + c] * wY[c];
-                out[1] += data.data[y * STRIDE + x * NC + c] * wCb[c];
-                out[2] += data.data[y * STRIDE + x * NC + c] * wCr[c];
-            }
-            for (int c = 0; c < NC; ++c) {
-                int val = (c != 0) ? 128 : 0;
-                data.data[y * STRIDE + x * NC + c] = out[c] + val;
-            }
+        for (int x = 0; x < w; ++x, p0 += NC, p1 += NC, p2 += NC) {
+            int B = *p0, G = *p1, R = *p2;
+
+            double Y = 0.299 * R + 0.587 * G + 0.114 * B;
+            double Cb = 0.1687 * R - 0.3313 * G + 0.5 * B + 128;
+            double Cr = 0.5 * R -0.4187 * G - 0.0813 * B + 128;
+
+            *p0 = static_cast<uchar>(roundl(Y));
+            *p1 = static_cast<uchar>(roundl(Cr));
+            *p2 = static_cast<uchar>(roundl(Cb));
         }
     }
 
